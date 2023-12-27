0327eccb8c5ea83ef0c8cd5135989dbc019fe748
Ekaterina Z <zotova.ea@gmail.com>
2023-12-27 19:54:29 +0100

    Ejercicio 3: tracking de chistes implementado

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 "use strict";
-
-const joke = document.getElementById("jokeFixed");
+const reportAcudits = [];
+let currentJoke;
+const jokeFixed = document.getElementById("jokeFixed");
 const button = document.querySelector("button");
-button.addEventListener('click', (e) => {
-    e.preventDefault();
-    mostrarDatosApi();
-});
+const vote1 = document.getElementById('1');
+const vote2 = document.getElementById('2');
+const vote3 = document.getElementById('3');
 function mostrarDatosApi() {
     fetch("https://icanhazdadjoke.com/", {
         headers: {
@@ -15,7 +15,38 @@ function mostrarDatosApi() {
         .then(res => res.json())
         .then(data => {
         console.log(data);
-        joke.innerHTML = `" ${data.joke} "`;
+        currentJoke = {
+            joke: data.joke,
+            score: 0,
+            date: new Date().toISOString(),
+        };
+        if (jokeFixed) {
+            jokeFixed.innerHTML = `" ${currentJoke.joke} "`;
+        }
+    });
+}
+vote1.addEventListener("click", (e) => {
+    e.preventDefault();
+    currentJoke.score = 1;
+    reportAcudits.push(currentJoke);
+    console.log(reportAcudits);
+});
+vote2.addEventListener("click", (e) => {
+    e.preventDefault();
+    currentJoke.score = 2;
+    reportAcudits.push(currentJoke);
+    console.log(reportAcudits);
+});
+vote3.addEventListener("click", (e) => {
+    e.preventDefault();
+    currentJoke.score = 3;
+    reportAcudits.push(currentJoke);
+    console.log(reportAcudits);
+});
+if (button) {
+    button.addEventListener('click', (e) => {
+        e.preventDefault();
+        mostrarDatosApi();
     });
 }
 mostrarDatosApi();
--- index.ts
+++ index.ts
@@ -1,36 +1,69 @@
+const reportAcudits: Joke[] = [];
+
+interface Joke {
+  joke: string;
+  score: number;
+  date: string;
+}
+
+let currentJoke: Joke;
+const jokeFixed: HTMLElement = document.getElementById("jokeFixed")!;
+const button: HTMLButtonElement = document.querySelector("button")!;
+
+const vote1: HTMLButtonElement = document.getElementById('1') as HTMLButtonElement;
+const vote2: HTMLButtonElement = document.getElementById('2') as HTMLButtonElement;
+const vote3: HTMLButtonElement = document.getElementById('3') as HTMLButtonElement;
+
+function mostrarDatosApi() {
+  fetch("https://icanhazdadjoke.com/", {
+    headers: {
+      "Accept": "application/json"
+    }
+  })
+    .then(res => res.json())
+    .then(data => {
+      console.log(data);
+      currentJoke = {
+        joke: data.joke,
+        score: 0,
+        date: new Date().toISOString(),
+      };
+
+      if (jokeFixed) {
+        jokeFixed.innerHTML = `" ${currentJoke.joke} "`;
+      }
 
 
-// En este primer ejercicio crearemos la pantalla principal que mostrará chistes al usuario/a.
-// El funcionamiento debe ser el siguiente:
-// - Al iniciar se mostrará el primer chiste por pantalla y el botón de siguiente chiste.
-// - Al pulsar el botón de “Siguiente chiste” se hará fetch a la API de chistes y se mostrará por consola y por pantalla el chiste.
-
-const joke: HTMLElement | null = document.getElementById("jokeFixed");
-const button: HTMLButtonElement | null = document.querySelector("button");
-
-function mostrarDatosApi(){
-    fetch("https://icanhazdadjoke.com/", {
-        headers: {
-            "Accept": "application/json"
-        }
-    })
-    .then (res => res.json())
-    .then (data=>{
-        console.log(data);
-        if (joke){
-           joke.innerHTML = data.joke; 
-        }
-        
-    })
+    });
 }
 
 
-if (button){
-    button.addEventListener('click', (e:MouseEvent)=>{
-        e.preventDefault();
-        mostrarDatosApi();
-     })
-}
+vote1.addEventListener("click", (e: MouseEvent) => {
+  e.preventDefault();
+  currentJoke.score = 1;
+  reportAcudits.push(currentJoke);
+  console.log(reportAcudits);
+});
 
+vote2.addEventListener("click", (e: MouseEvent) => {
+  e.preventDefault();
+  currentJoke.score = 2;
+  reportAcudits.push(currentJoke);
+  console.log(reportAcudits);
+});
+
+vote3.addEventListener("click", (e: MouseEvent) => {
+  e.preventDefault();
+  currentJoke.score = 3;
+  reportAcudits.push(currentJoke);
+  console.log(reportAcudits);
+});
+
+if (button) {
+  button.addEventListener('click', (e: MouseEvent) => {
+    e.preventDefault();
+    mostrarDatosApi();
+  });
+}
 
 mostrarDatosApi();
